1463fcac83268493c05c12c223d2d093ff5ec12f
srinivasansne2 <srinivasansne2@gmail.com>
2025-03-23 19:25:14 +0530

    First

---
+++ QS-Intern/JS/01.print.js
@@ -0,0 +1,6 @@
+console.log("Hello")
+var A=10;
+const C=30.13;
+console.log("A =",A)
+console.log("C =",C)
+console.log("A+C =",A+C)
---
+++ QS-Intern/JS/02.function.js
@@ -0,0 +1,10 @@
+// var a= function (){
+//     console.log("Functiolal Expression")
+// }
+// a()
+
+let a= ()=>{
+    return "Arrow";
+}
+let b=a()
+console.log(a)
