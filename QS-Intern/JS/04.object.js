3e5f9ed03435fbc04b0c1ed52f1f85e10fcdec12
srinivasansne2 <srinivasansne2@gmail.com>
2025-03-25 09:49:09 +0530

    d1

--- QS-Intern/JS/04.object.js
+++ QS-Intern/JS/04.object.js
@@ -1,6 +1,25 @@
+//Object
 let obj ={
     name:"Sri",
     age:21,
     phone:3322114455
 }
 console.log(obj);
+console.log(obj.phone); //to access the value
+obj.phone=1234567890;  //to update the value
+console.log(obj);
+obj.location="blr"    // Inserting Key & Value
+console.log(obj);
+delete obj.phone     // Deleting The Key & Value
+console.log(obj);
+
+// Nested Object
+let nest ={
+    name:"Sri",
+    age:21,
+    phone:{
+        Mobile:1236547890,
+        Tele:043282244335
+    }
+}
+console.log(nest);
