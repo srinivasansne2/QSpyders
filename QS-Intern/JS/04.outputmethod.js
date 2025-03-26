f72f0675f231b81da692e6be1ec79639c097091e
srinivasansne2 <srinivasansne2@gmail.com>
2025-03-26 09:50:51 +0530

    D2

---
+++ QS-Intern/JS/04.outputmethod.js
@@ -0,0 +1,16 @@
+// Alert
+alert("What Are You Doing")
+
+// Confirm
+confirm("Are You Fine?")
+
+// Document.Write / Writeln
+document.write("Hi")
+document.writeln("Hello")
+document.write("ok")
+
+
+// Prompt
+let a = Number(prompt("Enter The Value of a"))
+let b = Number(prompt("Enter The Value of b"))
+alert(a+b)
