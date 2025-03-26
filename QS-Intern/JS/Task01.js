9cee7b7802259da8d121aef566f265971ab9a78b
srinivasansne2 <srinivasansne2@gmail.com>
2025-03-26 10:10:06 +0530

    task

--- QS-Intern/JS/Task01.js
+++ QS-Intern/JS/Task01.js
@@ -1,7 +1,9 @@
-let a = Math.floor(Math.random()*10000)
-alert("Your OTP Is : " + a)
-let b= prompt("Enter The OTP")
-if (a==b) {
+// Simple OTP Login & Verify
+
+let otp = Math.floor(Math.random()*10000)
+alert("Your OTP Is : " + otp)
+let verify= prompt("Enter The OTP")
+if (otp==verify) {
     document.write("Login Succesfull")
 }
 else{
