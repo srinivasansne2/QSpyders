384c9ff6e04b11a20b5408c4a8e680452e33dcf8
srinivasansne2 <srinivasansne2@gmail.com>
2025-03-31 09:50:16 +0530

    ok

--- QS-Intern/JS/08.events.js
+++ QS-Intern/JS/08.events.js
@@ -1,6 +1,19 @@
-function x() {
-    document.body.style.backgroundColor="Red";
+//By Using onclick
+// ///In html file =<button onclick="x()">Click For Red</button>
+
+// function x() {
+//     document.body.style.backgroundColor="Red";
+//     console.log("Hello");
+    
+// }
+
+
+
+// By Using addEventListener
+let btn = document.querySelector("button")
+btn.addEventListener("click",function(){
     console.log("Hello");
     
-    
-}
+})
+
+
