2592921344d86842a70abd1d6071a78c3c6052c6
srinivasansne2 <srinivasansne2@gmail.com>
2025-04-23 10:02:25 +0530

    Adjust navbar styles and improve close button functionality for better usability

--- QS-Intern/Project/ecom.js
+++ QS-Intern/Project/ecom.js
@@ -2,12 +2,12 @@ let bar = document.getElementById("bar");
 let nav = document.getElementById("navbar");
 let close = document.getElementById("close");
 
-
 if (bar) {
     bar.addEventListener("click", () => {
         nav.classList.toggle("active");
     });
 }
+
 if (close) {
     close.addEventListener("click", () => {
         nav.classList.remove("active");
